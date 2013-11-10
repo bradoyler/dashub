079aa5d890792b7cfa9a1e9f78053d493073270d
Brad Oyler <bradoyler@gmail.com>
2013-11-09 23:00:34 -0500

    fuck fuck fuck heroku…

--- web.js
+++ web.js
@@ -2,11 +2,10 @@ var express = require("express");
 var app = express();
 app.use(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
+app.use(express.static(__dirname + '/dist'));
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
+  console.log("__dirname: " + __dirname);
 });
