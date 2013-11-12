5b9aebb01be08c14c1e3ca131f043668b702b329
Brad Oyler <bradoyler@gmail.com>
2013-11-12 11:48:19 -0500

    hello jshint

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,35 +3,36 @@ import getJSON from 'appkit/utils/get_json';
 import ajax from 'appkit/utils/ajax';
 
 var IndexRoute = Ember.Route.extend({
-  content:[],
+	content: [],
 
-  setupController: function(controller, model) {  
+	setupController: function(controller, model) {
 
-   	    //controller.set('model', model);
+		//controller.set('model', model);
 
-        var baseurl = 'https://api.github.com/repos/'+ $.cookie('reponame') +'/';
-        var self = this;
-        var authtoken = $.cookie('authtoken');
+		var baseurl = 'https://api.github.com/repos/' + $.cookie('reponame') + '/';
+		var self = this;
+		var authtoken = $.cookie('authtoken');
 
-        console.log("authtoken:", authtoken);
-    if(authtoken){
-        getJSON(baseurl+'issues')
-        .then(function(data) {
-        	self.controller.set('content', data);
-        });
+		console.log("authtoken:", authtoken);
+		if (authtoken) {
+			getJSON(baseurl + 'issues')
+				.then(function(data) {
+					self.controller.set('content', data);
+				});
 
-        getJSON(baseurl+'pulls?sort=updated&direction=desc')
-        .then(function(data) {
-        	self.controllerFor('pulls').set('content', data);
-        });
+			getJSON(baseurl + 'pulls?sort=updated&direction=desc')
+				.then(function(data) {
+					self.controllerFor('pulls').set('content', data);
+				});
 
-        getJSON(baseurl+'issues?state=closed&sort=updated')
-        .then(function(data) {
-        	self.controllerFor('closedissues').set('content', data);
-        });
-    }
-  }
+			getJSON(baseurl + 'issues?state=closed&sort=updated')
+				.then(function(data) {
+					self.controllerFor('closedissues').set('content', data);
+				});
+		}
+	}
 
 });
 
-export default IndexRoute;
+export
+default IndexRoute;
