1d02456f7110950dfbba278a098682f74dbbe772
Brad Oyler <bradoyler@gmail.com>
2013-11-17 20:10:08 -0500

    moved header to application.hbs + refactoring

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,27 +1,5 @@
 
 var IndexController = Ember.Controller.extend({
-	content: [],
-
-	reponame: function() {
-		return $.cookie('reponame');
-	}.property('reponame'),
-
-	repourl: function() {
-		return 'https://github.com/' + this.get('reponame') + '/issues';
-	}.property('reponame'),
-
-	authtoken: function() {
-		return $.cookie('authtoken');
-	}.property('authtoken'),
-
-	username: function() {
-		return $.cookie('username');
-	}.property('username'),
-
-	avatar_url: function() {
-		return $.cookie('avatar_url');
-	}.property('avatar_url')
-
 });
 
 export default IndexController;
